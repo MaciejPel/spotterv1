027b292b24622a81231a65d1b93cbade78dc6534
MaciejPel <maciejpelczynski0@gmail.com>
2023-09-09 12:16:34 +0200

    Profile - fix no available photos

--- frontend/src/components/AsideQueries.tsx
+++ frontend/src/components/AsideQueries.tsx
@@ -1,150 +1,162 @@
-import { useSearchParams } from 'react-router-dom';
-import { AsideType, AsideProps } from '../types/types';
-import useTheme from '../hooks/useTheme';
-import Tippy from '@tippyjs/react';
-import 'tippy.js/themes/light.css';
-import 'tippy.js/dist/tippy.css';
+import { useSearchParams } from "react-router-dom";
+import { AsideType, AsideProps } from "../types/types";
+import useTheme from "../hooks/useTheme";
+import Tippy from "@tippyjs/react";
+import "tippy.js/themes/light.css";
+import "tippy.js/dist/tippy.css";
 
 const queries = [
-	{
-		title: 'Time range',
-		description: 'Retrives data from specific time to current day',
-		values: ['short', 'medium', 'long'],
-		valuesDescription: ['Last 4 weeks', 'Last 6 months', 'Last few years'],
-		searchParam: 'time_range',
-		completeParam: '_term',
-		defaultValue: 0,
-		type: [AsideType.DATA, AsideType.GRAPH],
-	},
-	{
-		title: 'Number of records',
-		description: 'Number of returned records',
-		values: ['15', '25', '50'],
-		valuesDescription: ['15 records', '25 records', '50 records'],
-		searchParam: 'limit',
-		completeParam: '',
-		defaultValue: 1,
-		type: [AsideType.DATA],
-	},
+  {
+    title: "Time range",
+    description: "Retrives data from specific time to current day",
+    values: ["short", "medium", "long"],
+    valuesDescription: ["Last 4 weeks", "Last 6 months", "Last few years"],
+    searchParam: "time_range",
+    completeParam: "_term",
+    defaultValue: 0,
+    type: [AsideType.DATA, AsideType.GRAPH],
+  },
+  {
+    title: "Number of records",
+    description: "Number of returned records",
+    values: ["15", "25", "50"],
+    valuesDescription: ["15 records", "25 records", "50 records"],
+    searchParam: "limit",
+    completeParam: "",
+    defaultValue: 1,
+    type: [AsideType.DATA],
+  },
 ];
 
 const sizeOptions = {
-	title: 'Number of records',
-	description: 'Number of returned records',
-	values: ['10', '20', 'All'],
-	actualValues: [10, 20, -1],
-	valuesDescription: ['10 records', '20 records', 'All records'],
-	defaultValue: 10,
+  title: "Number of records",
+  description: "Number of returned records",
+  values: ["10", "20", "All"],
+  actualValues: [10, 20, -1],
+  valuesDescription: ["10 records", "20 records", "All records"],
+  defaultValue: 10,
 };
 
 const legendOptions = {
-	title: 'Legend',
-	values: ['Yes', 'No'],
-	actualValues: [true, false],
-	valuesDescription: ['Yes', 'No'],
-	defaultValue: false,
+  title: "Legend",
+  values: ["Yes", "No"],
+  actualValues: [true, false],
+  valuesDescription: ["Yes", "No"],
+  defaultValue: false,
 };
 
-const AsideQueries: React.FC<AsideProps> = ({ type, size, setSize, legend, setLegend }) => {
-	const [searchParams, setSearchParams] = useSearchParams();
-	const { currentMode } = useTheme();
+const AsideQueries: React.FC<AsideProps> = ({
+  type,
+  size,
+  setSize,
+  legend,
+  setLegend,
+}) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { currentMode } = useTheme();
 
-	return (
-		<aside className="sidebar">
-			{queries
-				.filter((query) => query.type.includes(type))
-				.map((query, i) => (
-					<div className="sidebar__section" key={i}>
-						<Tippy
-							content={query.description}
-							theme={currentMode}
-							arrow={false}
-							placement={'bottom'}
-						>
-							<div className="sidebar__section__title">{query.title}</div>
-						</Tippy>
-						<div className="sidebar__section__options">
-							{query.values.map((value, index) => (
-								<Tippy
-									content={query.valuesDescription[index]}
-									theme={currentMode}
-									arrow={false}
-									key={index}
-								>
-									<button
-										onClick={() => {
-											index === query.defaultValue
-												? searchParams.delete(query.searchParam)
-												: searchParams.set(query.searchParam, value + query.completeParam);
-											setSearchParams(searchParams);
-										}}
-										className={`sidebar__button ${
-											searchParams.get(query.searchParam) === value + query.completeParam ||
-											(!searchParams.get(query.searchParam) && index === query.defaultValue)
-												? 'active'
-												: ''
-										}`}
-									>
-										{value[0].toUpperCase() + value.slice(1)}
-									</button>
-								</Tippy>
-							))}
-						</div>
-					</div>
-				))}
-			{type === AsideType.GRAPH && (
-				<>
-					<div className="sidebar__section">
-						<Tippy
-							content={sizeOptions.description}
-							theme={currentMode}
-							arrow={false}
-							placement={'bottom'}
-						>
-							<div className="sidebar__section__title">{sizeOptions.title}</div>
-						</Tippy>
-						<div className="sidebar__section__options">
-							{sizeOptions.values.map((value, index: number) => (
-								<Tippy
-									content={sizeOptions.valuesDescription[index]}
-									theme={currentMode}
-									arrow={false}
-									key={index}
-								>
-									<button
-										onClick={() => {
-											setSize && setSize(sizeOptions.actualValues[index]);
-										}}
-										className={`sidebar__button ${
-											size === sizeOptions.actualValues[index] ? 'active' : ''
-										}`}
-									>
-										{value}
-									</button>
-								</Tippy>
-							))}
-						</div>
-					</div>
-					<div className="sidebar__section">
-						<div className="sidebar__section__title">{legendOptions.title}</div>
-						<div className="sidebar__section__options">
-							{legendOptions.values.map((value, index: number) => (
-								<button
-									onClick={() => {
-										setLegend && setLegend(legendOptions.actualValues[index]);
-									}}
-									className={`sidebar__button ${
-										legend === legendOptions.actualValues[index] ? 'active' : ''
-									}`}
-								>
-									{value}
-								</button>
-							))}
-						</div>
-					</div>
-				</>
-			)}
-		</aside>
-	);
+  return (
+    <aside className="sidebar">
+      {queries
+        .filter((query) => query.type.includes(type))
+        .map((query, i) => (
+          <div className="sidebar__section" key={i}>
+            <Tippy
+              content={query.description}
+              theme={currentMode}
+              arrow={false}
+              placement={"bottom"}
+            >
+              <div className="sidebar__section__title">{query.title}</div>
+            </Tippy>
+            <div className="sidebar__section__options">
+              {query.values.map((value, index) => (
+                <Tippy
+                  content={query.valuesDescription[index]}
+                  theme={currentMode}
+                  arrow={false}
+                  key={index}
+                >
+                  <button
+                    onClick={() => {
+                      index === query.defaultValue
+                        ? searchParams.delete(query.searchParam)
+                        : searchParams.set(
+                            query.searchParam,
+                            value + query.completeParam
+                          );
+                      setSearchParams(searchParams);
+                    }}
+                    className={`sidebar__button ${
+                      searchParams.get(query.searchParam) ===
+                        value + query.completeParam ||
+                      (!searchParams.get(query.searchParam) &&
+                        index === query.defaultValue)
+                        ? "active"
+                        : ""
+                    }`}
+                  >
+                    {value[0].toUpperCase() + value.slice(1)}
+                  </button>
+                </Tippy>
+              ))}
+            </div>
+          </div>
+        ))}
+      {type === AsideType.GRAPH && (
+        <>
+          <div className="sidebar__section">
+            <Tippy
+              content={sizeOptions.description}
+              theme={currentMode}
+              arrow={false}
+              placement={"bottom"}
+            >
+              <div className="sidebar__section__title">{sizeOptions.title}</div>
+            </Tippy>
+            <div className="sidebar__section__options">
+              {sizeOptions.values.map((value, index: number) => (
+                <Tippy
+                  content={sizeOptions.valuesDescription[index]}
+                  theme={currentMode}
+                  arrow={false}
+                  key={index}
+                >
+                  <button
+                    onClick={() => {
+                      setSize && setSize(sizeOptions.actualValues[index]);
+                    }}
+                    className={`sidebar__button ${
+                      size === sizeOptions.actualValues[index] ? "active" : ""
+                    }`}
+                  >
+                    {value}
+                  </button>
+                </Tippy>
+              ))}
+            </div>
+          </div>
+          <div className="sidebar__section">
+            <div className="sidebar__section__title">{legendOptions.title}</div>
+            <div className="sidebar__section__options">
+              {legendOptions.values.map((value, index: number) => (
+                <button
+                  onClick={() => {
+                    setLegend && setLegend(legendOptions.actualValues[index]);
+                  }}
+                  className={`sidebar__button ${
+                    legend === legendOptions.actualValues[index] ? "active" : ""
+                  }`}
+                  key={index}
+                >
+                  {value}
+                </button>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </aside>
+  );
 };
 export default AsideQueries;
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,62 +1,73 @@
-import { useQuery } from 'react-query';
-import { useDispatch } from 'react-redux';
-import { resetAuth } from '../features/auth/authSlice';
-import { useNavigate } from 'react-router-dom';
-import { fetchSpotifyAPI } from '../utils/spotifyApi';
-import useAuth from '../hooks/useAuth';
+import { useQuery } from "react-query";
+import { useDispatch } from "react-redux";
+import { resetAuth } from "../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+import { fetchSpotifyAPI } from "../utils/spotifyApi";
+import { BiUser } from "react-icons/bi";
+import useAuth from "../hooks/useAuth";
 
 const Profile: React.FC = () => {
-	document.title = 'Profile | Spotter';
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
-	const { accessToken } = useAuth();
-	const { data } = useQuery('userName', () => fetchSpotifyAPI('/me', accessToken), {
-		refetchOnWindowFocus: false,
-		enabled: accessToken ? true : false,
-	});
+  document.title = "Profile | Spotter";
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { accessToken } = useAuth();
+  const { data } = useQuery("userName", () => fetchSpotifyAPI("/me", accessToken), {
+    refetchOnWindowFocus: false,
+    enabled: accessToken ? true : false,
+  });
 
-	const handleLogout = () => {
-		dispatch(resetAuth());
-		navigate('/');
-	};
+  const handleLogout = () => {
+    dispatch(resetAuth());
+    navigate("/");
+  };
 
-	return (
-		<div className="profile">
-			<div className="profile__card">
-				<div className="profile__image__wrapper">
-					<img className="profile__image" src={data && data.images[0].url} alt="Profile" />
-				</div>
-				<div className="profile__info">
-					<div className="profile__name">You're logged in as {data && data.display_name}</div>
-					<div className="profile__id">
-						ID: <span>{data && data.id}</span>
-					</div>
-					<div className="profile__email">
-						Email: <span>{data && data.email}</span>
-					</div>
-					<div className="profile__country">
-						Country: <span>{data && data.country}</span>
-					</div>
-					<div className="profile__followers">
-						Number of followers: <span>{data && data.followers.total}</span>
-					</div>
-					<div className="profile__buttons">
-						<a
-							className="profile__button profile__spotify"
-							href="https://www.spotify.com/account/overview/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							Spotify account
-						</a>
-						<button className="profile__button profile__logout" onClick={handleLogout}>
-							Log me out 🏃🏽‍♂️
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="profile">
+      <div className="profile__card">
+        <div className="profile__image__wrapper">
+          {!data ? null : data.images.length ? (
+            <img
+              className="profile__image"
+              src={data.images[data.images.length - 1].url}
+              alt="Profile"
+            />
+          ) : (
+            <BiUser className="profile__image" style={{ width: "16rem" }} />
+          )}
+        </div>
+        <div className="profile__info">
+          <div className="profile__name">
+            You're logged in as {data && data.display_name}
+          </div>
+          <div className="profile__id">
+            ID: <span>{data && data.id}</span>
+          </div>
+          <div className="profile__email">
+            Email: <span>{data && data.email}</span>
+          </div>
+          <div className="profile__country">
+            Country: <span>{data && data.country}</span>
+          </div>
+          <div className="profile__followers">
+            Number of followers: <span>{data && data.followers.total}</span>
+          </div>
+          <div className="profile__buttons">
+            <a
+              className="profile__button profile__spotify"
+              href="https://www.spotify.com/account/overview/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Spotify account
+            </a>
+            <button className="profile__button profile__logout" onClick={handleLogout}>
+              Log me out 🏃🏽‍♂️
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
